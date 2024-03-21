830015c4de52485ca286375cea37222eebc24c96
JanainaLeal <73653429+JanainaLeal@users.noreply.github.com>
2024-03-21 12:00:02 +0000

    User classes

--- src/Recomecar/WebApiDB/Models/User.cs
+++ src/Recomecar/WebApiDB/Models/User.cs
@@ -1,6 +1,12 @@
 ﻿namespace WebApiDB.Models
 {
     public class User
-    {
-    }
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }       
+        public string Image { get; set; }
+        public User() { }
+    }   
 }
