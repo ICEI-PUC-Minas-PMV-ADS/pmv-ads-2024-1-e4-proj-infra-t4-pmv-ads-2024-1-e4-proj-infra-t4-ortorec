4e8665d8eb1d5185adf64ef2280380f5cfd3a085
sil-barbosa <ma.dosil82@gmail.com>
2024-05-12 20:14:37 -0300

    Integração Front/Back  Produtos (falta estilizar a página)

--- src/APIPedidosRecomecar/APIPedidosRecomecar/obj/Release/net8.0/APIPedidosRecomecar.AssemblyInfo.cs
+++ src/APIPedidosRecomecar/APIPedidosRecomecar/obj/Release/net8.0/APIPedidosRecomecar.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIPedidosRecomecar")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb06d41df179aa8ab1ddab3872660edf3d40742d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIPedidosRecomecar")]
