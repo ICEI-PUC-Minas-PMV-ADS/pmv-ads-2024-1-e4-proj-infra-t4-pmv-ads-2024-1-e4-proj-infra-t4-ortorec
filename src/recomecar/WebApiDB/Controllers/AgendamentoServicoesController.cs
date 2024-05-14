6bf77efa1f2d88d2f6e6c599ada69d7419017642
JanainaLeal <73653429+JanainaLeal@users.noreply.github.com>
2024-05-14 11:11:24 +0100

    Recuperaçao de Controladores

---
+++ src/recomecar/WebApiDB/Controllers/AgendamentoServicoesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApiDB.Data;
+using WebApiDB.Models;
+
+namespace WebApiDB.Controllers
+{
+    public class AgendamentoServicoesController : Controller
+    {
+        private readonly RecomecarDBContext _context;
+
+        public AgendamentoServicoesController(RecomecarDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AgendamentoServicoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AgendamentoServicos.ToListAsync());
+        }
+
+        // GET: AgendamentoServicoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agendamentoServico = await _context.AgendamentoServicos
+                .FirstOrDefaultAsync(m => m.AgendamentoServicoId == id);
+            if (agendamentoServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(agendamentoServico);
+        }
+
+        // GET: AgendamentoServicoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AgendamentoServicoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AgendamentoServicoId,DataAgendamento")] AgendamentoServico agendamentoServico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(agendamentoServico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(agendamentoServico);
+        }
+
+        // GET: AgendamentoServicoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agendamentoServico = await _context.AgendamentoServicos.FindAsync(id);
+            if (agendamentoServico == null)
+            {
+                return NotFound();
+            }
+            return View(agendamentoServico);
+        }
+
+        // POST: AgendamentoServicoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AgendamentoServicoId,DataAgendamento")] AgendamentoServico agendamentoServico)
+        {
+            if (id != agendamentoServico.AgendamentoServicoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(agendamentoServico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AgendamentoServicoExists(agendamentoServico.AgendamentoServicoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(agendamentoServico);
+        }
+
+        // GET: AgendamentoServicoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agendamentoServico = await _context.AgendamentoServicos
+                .FirstOrDefaultAsync(m => m.AgendamentoServicoId == id);
+            if (agendamentoServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(agendamentoServico);
+        }
+
+        // POST: AgendamentoServicoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var agendamentoServico = await _context.AgendamentoServicos.FindAsync(id);
+            if (agendamentoServico != null)
+            {
+                _context.AgendamentoServicos.Remove(agendamentoServico);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AgendamentoServicoExists(int id)
+        {
+            return _context.AgendamentoServicos.Any(e => e.AgendamentoServicoId == id);
+        }
+    }
+}
---
+++ src/recomecar/WebApiDB/Controllers/ServicoesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApiDB.Data;
+using WebApiDB.Models;
+
+namespace WebApiDB.Controllers
+{
+    public class ServicoesController : Controller
+    {
+        private readonly RecomecarDBContext _context;
+
+        public ServicoesController(RecomecarDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Servicos.ToListAsync());
+        }
+
+        // GET: Servicoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(m => m.ServicoId == id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+
+            return View(servico);
+        }
+
+        // GET: Servicoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServicoId,Descricao,Pago,Preco,Imagem")] Servico servico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servico);
+        }
+
+        // GET: Servicoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos.FindAsync(id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+            return View(servico);
+        }
+
+        // POST: Servicoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ServicoId,Descricao,Pago,Preco,Imagem")] Servico servico)
+        {
+            if (id != servico.ServicoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicoExists(servico.ServicoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servico);
+        }
+
+        // GET: Servicoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(m => m.ServicoId == id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+
+            return View(servico);
+        }
+
+        // POST: Servicoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var servico = await _context.Servicos.FindAsync(id);
+            if (servico != null)
+            {
+                _context.Servicos.Remove(servico);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicoExists(int id)
+        {
+            return _context.Servicos.Any(e => e.ServicoId == id);
+        }
+    }
+}
