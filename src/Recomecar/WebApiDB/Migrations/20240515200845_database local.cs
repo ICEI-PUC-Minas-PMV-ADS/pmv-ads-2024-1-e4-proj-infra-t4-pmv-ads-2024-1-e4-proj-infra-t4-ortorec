d2136cd403db2353f6da4e4c083ba285c3f11e23
JanainaLeal <73653429+JanainaLeal@users.noreply.github.com>
2024-05-15 22:17:55 +0100

    Atualizaçao para o banco de dados Local

---
+++ src/Recomecar/WebApiDB/Migrations/20240515200845_database local.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class databaselocal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
